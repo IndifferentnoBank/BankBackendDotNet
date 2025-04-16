dc426de930117f438b51783dfe25b179133f112f
Vasandr15 <vasandrey007@gmail.com>
2025-04-16 22:00:18 +0700

    merged

--- UserService.Presentation/obj/Debug/net9.0/UserService.Presentation.AssemblyInfo.cs
+++ UserService.Presentation/obj/Debug/net9.0/UserService.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684caef2e44c61bd32bb6300ea3a1457ca43560e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f9638620dab7025956691c89739f21f4b7e736")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
