cfa3dc594de77cd258d3d1edf48aa68b73fb6058
ilyainavoid <thirdilno@gmail.com>
2025-04-20 01:57:26 +0700

    SignalR context fix

--- CoreService.Contracts/Interfaces/ITransactionHub.cs
+++ CoreService.Contracts/Interfaces/ITransactionHub.cs
@@ -4,8 +4,6 @@ namespace CoreService.Contracts.Interfaces;
 
 public interface ITransactionHub
 {
-    public Task SendTransactionUpdate(TransactionDto transaction);
-    public Task SendTransactionUpdateToBankAccount(Guid bankAccountId, TransactionDto transaction);
     public Task JoinAllTransactionsGroup();
     public Task LeaveAllTransactionsGroup();
     public Task JoinBankAccountGroup(Guid bankAccountId);
---
+++ CoreService.Infrastructure/SignalR/TransactionNotifier.cs
@@ -0,0 +1,35 @@
+﻿using CoreService.Contracts.ExternalDtos;
+using CoreService.Contracts.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CoreService.Infrastructure.SignalR;
+
+public class TransactionNotifier : ITransactionNotifier
+{
+    private readonly IHubContext<TransactionHub> _hubContext;
+
+    private const string AllTransactionsGroup = "AllTransactions";
+
+    public TransactionNotifier(IHubContext<TransactionHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task SendTransactionUpdate(TransactionDto transaction)
+    {
+        await _hubContext.Clients.Group(AllTransactionsGroup)
+            .SendAsync("ReceiveTransactionUpdate", transaction);
+    }
+
+    public async Task SendTransactionUpdateToBankAccount(Guid bankAccountId, TransactionDto transaction)
+    {
+        string groupName = GetGroupNameForBankAccount(bankAccountId);
+        await _hubContext.Clients.Group(groupName)
+            .SendAsync("ReceiveTransactionUpdateByBankAccountId", transaction);
+    }
+
+    private static string GetGroupNameForBankAccount(Guid bankAccountId)
+    {
+        return $"BankAccount:{bankAccountId}";
+    }
+}
