46f98fe8e4d4c3052409eacddf152fcf6700f78c
Дмитрий Фофанцев <kfk.551@yandex.ru>
2025-04-23 23:11:36 +0700

    fix

--- CreditRatingService.Persistence/CreditRatingServicePersistenceConfiguration.cs
+++ CreditRatingService.Persistence/CreditRatingServicePersistenceConfiguration.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using UserService.Persistence.Repositories.ExpiredTokensRepository;
+using CreditRatingService.Persistence.Repositories.ExpiredTokensRepository;
 
 namespace CreditRatingService.Persistence
 {
